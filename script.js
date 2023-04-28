64408511dba84295d9aed6d961aec7a98367e72d
jpisungu <jpisungu@redcat.io>
2023-04-28 02:05:32 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+$('#searchButton').click(function () {
+    let location = $('#searchInput').val();
+    callApi(location);
+});
+
+$(document).keypress(function(event){
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if(keycode == '13'){
+        let location = $('#searchInput').val();
+        callApi(location);
+    }
+});
+
+function callApi(location) {
+    let string_norm  = location.normalize('NFD').replace(/\p{Diacritic}/gu, '');
+    $.ajax({
+        url: "http://api.weatherapi.com/v1/current.json?key=862defbfa1764dd4b10220355232704&q="+string_norm+"&aqi=yes"
+    }).done(function (data) {
+        console.log(data);
+    });
+}
